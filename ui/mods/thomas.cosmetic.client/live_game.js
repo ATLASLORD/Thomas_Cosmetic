ad9755156f776d64a2389a1ca6e92b0295dc70cf
ATLASLORD <104906253+ATLASLORD@users.noreply.github.com>
2023-02-17 00:26:15 +0100

    useless sounds removed icon fixed

--- ui/mods/thomas.cosmetic.client/live_game.js
+++ ui/mods/thomas.cosmetic.client/live_game.js
@@ -6,25 +6,19 @@ try
       
       var launch =
       [
-        '01.ogg',
-        '02.ogg'
+        '01.ogg'
       ];
       
       var battle =
       [
-        '03.ogg',
-        '04.ogg',
-        '05.ogg',
-        '07.ogg'
+        '03.ogg'
       ];
 
       var base =
       [
-        '08.ogg',
         '09.ogg',
         '10.ogg',
         '11.ogg',
-        '12.ogg',
         '13.ogg'
       ];
 
@@ -35,8 +29,8 @@ try
       api.audio.registerCueMod('/Music/Music_Base', dir + _.sample(base));
 
       //Win & Lose music
-      api.audio.registerCueMod('/Music/Music_Gameover_Win', dir + '06.ogg');
-      api.audio.registerCueMod('/Music/Music_GW_Win', dir + '06.ogg');
+      api.audio.registerCueMod('/Music/Music_Gameover_Win', dir + '05.ogg');
+      api.audio.registerCueMod('/Music/Music_GW_Win', dir + '05.ogg');
 
       api.audio.registerCueMod('/Music/Music_Gameover_Lose', dir + '13.ogg');
       api.audio.registerCueMod('/Music/Music_Commander_Death', dir + '13.ogg');
@@ -47,10 +41,10 @@ try
       api.audio.registerCueMod('/Music/Music_Planet_Load_Ice', dir + '17.ogg');
       api.audio.registerCueMod('/Music/Music_Planet_Load_Metal', dir + '16.ogg');
       api.audio.registerCueMod('/Music/Music_Planet_Load_Tropical', dir + '15.ogg');
-      api.audio.registerCueMod('/Music/Music_Planet_Load_Lava', dir + '14.ogg');
+      api.audio.registerCueMod('/Music/Music_Planet_Load_Lava', dir + '15.ogg');
       api.audio.registerCueMod('/Music/Music_Planet_Load_Earth', dir + '16.ogg');
       api.audio.registerCueMod('/Music/Music_Planet_Load_Moon', dir + '15.ogg');
-      api.audio.registerCueMod('/Music/Music_Planet_Load_water', dir + '12.ogg');
+      api.audio.registerCueMod('/Music/Music_Planet_Load_water', dir + '16.ogg');
 
     }
     _.defer(audioModdingTest); // hack relies on synchronous code to defer register until after reset
